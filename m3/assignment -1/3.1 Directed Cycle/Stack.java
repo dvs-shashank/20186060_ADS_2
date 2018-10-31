cf9c3f981991a7122b2797b5b400195adc2a077e
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-10-31 17:48:47 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/assignment -1/3.1 Directed Cycle/Stack.java
+++ m3/assignment -1/3.1 Directed Cycle/Stack.java
@@ -4,8 +4,8 @@
  *
  *  A generic stack, implemented using a linked list. Each stack
  *  element is of type Item.
- *  
- *  % more tobe.txt 
+ *
+ *  % more tobe.txt
  *  to be or not to - be - - that - - - is
  *
  *  % java Stack < tobe.txt
@@ -38,31 +38,31 @@ class Stack<Item> implements Iterable<Item> {
         private Node next;
     }
 
-   /**
-     * Create an empty stack.
-     */
+    /**
+      * Create an empty stack.
+      */
     public Stack() {
         first = null;
         N = 0;
     }
 
-   /**
-     * Is the stack empty?
-     */
+    /**
+      * Is the stack empty?
+      */
     public boolean isEmpty() {
         return first == null;
     }
 
-   /**
-     * Return the number of items in the stack.
-     */
+    /**
+      * Return the number of items in the stack.
+      */
     public int size() {
         return N;
     }
 
-   /**
-     * Add the item to the stack.
-     */
+    /**
+      * Add the item to the stack.
+      */
     public void push(Item item) {
         Node oldfirst = first;
         first = new Node();
@@ -71,10 +71,10 @@ class Stack<Item> implements Iterable<Item> {
         N++;
     }
 
-   /**
-     * Delete and return the item most recently added to the stack.
-     * Throw an exception if no such item exists because the stack is empty.
-     */
+    /**
+      * Delete and return the item most recently added to the stack.
+      * Throw an exception if no such item exists because the stack is empty.
+      */
     public Item pop() {
         if (isEmpty()) throw new RuntimeException("Stack underflow");
         Item item = first.item;        // save item to return
@@ -84,29 +84,29 @@ class Stack<Item> implements Iterable<Item> {
     }
 
 
-   /**
-     * Return the item most recently added to the stack.
-     * Throw an exception if no such item exists because the stack is empty.
-     */
+    /**
+      * Return the item most recently added to the stack.
+      * Throw an exception if no such item exists because the stack is empty.
+      */
     public Item peek() {
         if (isEmpty()) throw new RuntimeException("Stack underflow");
         return first.item;
     }
 
-   /**
-     * Return string representation.
-     */
+    /**
+      * Return string representation.
+      */
     public String toString() {
         StringBuilder s = new StringBuilder();
         for (Item item : this)
             s.append(item + " ");
         return s.toString();
     }
-       
 
-   /**
-     * Return an iterator to the stack that iterates through the items in LIFO order.
-     */
+
+    /**
+      * Return an iterator to the stack that iterates through the items in LIFO order.
+      */
     public Iterator<Item> iterator()  { return new ListIterator();  }
 
     // an iterator, doesn't implement remove() since it's optional
@@ -118,23 +118,23 @@ class Stack<Item> implements Iterable<Item> {
         public Item next() {
             if (!hasNext()) throw new NoSuchElementException();
             Item item = current.item;
-            current = current.next; 
+            current = current.next;
             return item;
         }
     }
 
 
-   /**
-     * A test client.
-     */
-/*    public static void main(String[] args) {
-        Stack<String> s = new Stack<String>();
-        while (!StdIn.isEmpty()) {
-            String item = StdIn.readString();
-            if (!item.equals("-")) s.push(item);
-            else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
-        }
-        StdOut.println("(" + s.size() + " left on stack)");
-    }*/
+    /**
+      * A test client.
+      */
+    /*    public static void main(String[] args) {
+            Stack<String> s = new Stack<String>();
+            while (!StdIn.isEmpty()) {
+                String item = StdIn.readString();
+                if (!item.equals("-")) s.push(item);
+                else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+            }
+            StdOut.println("(" + s.size() + " left on stack)");
+        }*/
 }
 
