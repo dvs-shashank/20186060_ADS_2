70790a0e9a5b62a51a0b5bb7690dac8b0cc52abf
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-10-31 18:47:48 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/assignment -1/3.1 Directed Cycle/Stack.java
+++ m3/assignment -1/3.1 Directed Cycle/Stack.java
@@ -30,7 +30,7 @@ class Stack<Item> implements Iterable<Item> {
     /**
      * Constructs the object.
      */
-    public Stack() {
+    Stack() {
         first = null;
         n = 0;
     }
@@ -137,7 +137,9 @@ class Stack<Item> implements Iterable<Item> {
          * @return     { description_of_the_return_value }.
          */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
             Item item = current.item;
             current = current.next;
             return item;
