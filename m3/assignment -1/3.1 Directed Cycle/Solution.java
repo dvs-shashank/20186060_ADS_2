56dc72cb4e230405df701eecfaa864c419cfad05
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-10-31 14:39:44 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m3/assignment -1/3.1 Directed Cycle/Solution.java
+++ m3/assignment -1/3.1 Directed Cycle/Solution.java
@@ -3,30 +3,28 @@ import java.util.Scanner;
  * class Solution.
  */
 public final class Solution {
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int numOfVertices = Integer.parseInt(scan.nextLine());
-		int numOfEdges = Integer.parseInt(scan.nextLine());
-		Digraph dg = new Digraph(numOfVertices);
-		while (numOfEdges > 0) {
-			String[] connectedVertices = scan.nextLine().split(" ");
-			dg.addEdge(Integer.parseInt(connectedVertices[0]),
-				Integer.parseInt(connectedVertices[1]));
-			numOfEdges--;
-		}
-		DirectedCycle dc = new DirectedCycle(dg);
-		// boolean something = dc.hasCycle();
-		// System.out.println(something);
-		if (dc.hasCycle()) {
-			System.out.println("Cycle exists.");
-		} else {
-			System.out.println("Cycle doesn't exists.");
-		}
-
-	}
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int numOfVertices = Integer.parseInt(scan.nextLine());
+        int numOfEdges = Integer.parseInt(scan.nextLine());
+        Digraph dg = new Digraph(numOfVertices);
+        while (numOfEdges > 0) {
+            String[] connectedVertices = scan.nextLine().split(" ");
+            dg.addEdge(Integer.parseInt(connectedVertices[0]),
+                       Integer.parseInt(connectedVertices[1]));
+            numOfEdges--;
+        }
+        DirectedCycle dc = new DirectedCycle(dg);
+        if (dc.hasCycle()) {
+            System.out.println("Cycle exists.");
+        } else {
+            System.out.println("Cycle doesn't exists.");
+        }
+    }
 }
+
