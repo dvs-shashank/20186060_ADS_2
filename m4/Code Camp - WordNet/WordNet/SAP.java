147ad91582de072496028e7641e255faeb856c64
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-02 18:58:54 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m4/Code Camp - WordNet/WordNet/SAP.java
+++ m4/Code Camp - WordNet/WordNet/SAP.java
@@ -7,44 +7,38 @@ public class SAP {
 
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        int shortestPath = 99999999;
-        int j = 0;
+        //int j = 0;
         BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
         BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
+        int shortestPath = Integer.MAX_VALUE;
         for (int i = 0; i < graph.V(); i++) {
             if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
                 int distance = bfsForV.distTo(i) + bfsForW.distTo(i);
-                if (distance > j && distance < shortestPath) {
+                if (distance < shortestPath) {
                     shortestPath = distance;
                 }
             }
         }
-        if (shortestPath > 0) {
-            return shortestPath;
-        }
-        return -1;
+        return shortestPath;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
         BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
         BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
-        int shortestPath = 99999999;
-        int j = 0;
+        int shortestPath = Integer.MAX_VALUE;
+        //int j = 0;
         int ancestor = 0;
         for (int i = 0; i < graph.V(); i++) {
             if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
                 int distance = bfsForV.distTo(i) + bfsForW.distTo(i);
-                if (distance > j && distance < shortestPath) {
+                if (distance < shortestPath) {
                     shortestPath = distance;
                     ancestor = i;
                 }
             }
         }
-        if (ancestor > 0) {
-            return ancestor;
-        }
-        return -1;
+        return ancestor;
     }
 
     // do unit testing of this class
