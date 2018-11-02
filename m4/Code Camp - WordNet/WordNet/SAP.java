9c7d14b84b356c5ef85d234f02c2c86360031384
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-02 19:01:06 +0530

    "ads2_m4_a1_1: 7/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m4/Code Camp - WordNet/WordNet/SAP.java
+++ m4/Code Camp - WordNet/WordNet/SAP.java
@@ -1,46 +1,38 @@
 public class SAP {
     private Digraph graph;
     // constructor takes a digraph (not necessarily a DAG)
-    public SAP(Digraph digraph) {
-        this.graph = digraph;
-    }
+    int distanceGlobal;
 
+    public SAP(Digraph graph) {
+        this.graph = graph;
+        this.distanceGlobal = 0;
+    }
     // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
     public int length(Iterable<Integer> v, Iterable<Integer> w) {
-        //int j = 0;
-        BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
-        BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
-        int shortestPath = Integer.MAX_VALUE;
-        for (int i = 0; i < graph.V(); i++) {
-            if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
-                int distance = bfsForV.distTo(i) + bfsForW.distTo(i);
-                if (distance < shortestPath) {
-                    shortestPath = distance;
-                }
-            }
-        }
-        return shortestPath;
+        ancestor(v, w);
+        return distanceGlobal;
     }
 
     // a common ancestor that participates in shortest ancestral path; -1 if no such path
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w) {
-        BreadthFirstDirectedPaths bfsForV = new BreadthFirstDirectedPaths(graph, v);
-        BreadthFirstDirectedPaths bfsForW = new BreadthFirstDirectedPaths(graph, w);
-        int shortestPath = Integer.MAX_VALUE;
-        //int j = 0;
-        int ancestor = 0;
+        BreadthFirstDirectedPaths bfsV = new BreadthFirstDirectedPaths(graph, v);
+        BreadthFirstDirectedPaths bfsW = new BreadthFirstDirectedPaths(graph, w);
+        distanceGlobal = Integer.MAX_VALUE;
+        int ancestors = -1;
         for (int i = 0; i < graph.V(); i++) {
-            if (bfsForV.hasPathTo(i) && bfsForW.hasPathTo(i)) {
-                int distance = bfsForV.distTo(i) + bfsForW.distTo(i);
-                if (distance < shortestPath) {
-                    shortestPath = distance;
-                    ancestor = i;
+            if (bfsV.hasPathTo(i) && bfsW.hasPathTo(i)) {
+                int distance = bfsV.distTo(i) + bfsW.distTo(i);
+                if (distance < distanceGlobal) {
+                    //shortPath = distance;
+                    distanceGlobal = distance;
+                    ancestors = i;
                 }
+                // return ancestor;
             }
         }
-        return ancestor;
+        return ancestors;
     }
 
-    // do unit testing of this class
-    //public static void main(String[] args)
+    // // do unit testing of this class
+    // public static void main(String[] args)
 }
