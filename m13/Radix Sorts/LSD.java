45a3795124480cf7e389d304aad531ddaec6950b
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-12 15:01:57 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m13/Radix Sorts/LSD.java
+++ m13/Radix Sorts/LSD.java
@@ -21,20 +21,20 @@ public class LSD {
       */
     public static void sort(final String[] a, final int w) {
         int n = a.length;
-        int R = 256;   // extend ASCII alphabet size
+        int capitalR = 256;   // extend ASCII alphabet size
         String[] aux = new String[n];
 
         for (int d = w - 1; d >= 0; d--) {
             // sort by key-indexed counting on dth character
 
             // compute frequency counts
-            int[] count = new int[R + 1];
+            int[] count = new int[capitalR + 1];
             for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
             }
 
             // compute cumulates
-            for (int r = 0; r < R; r++) {
+            for (int r = 0; r < capitalR; r++) {
                 count[r + 1] += count[r];
             }
 
@@ -57,10 +57,10 @@ public class LSD {
       * @param a the array to be sorted
       */
     public static void sort(final int[] a) {
-        final int BITS = 32;                 // each int is 32 bits
+        final int bits = 32;                 // each int is 32 bits
         final int R = 1 << BITS_PER_BYTE; // each bytes is between 0 and 255
-        final int MASK = R - 1;              // 0xFF
-        final int w = BITS / BITS_PER_BYTE;  // each int is 4 bytes
+        final int mask = R - 1;              // 0xFF
+        final int w = bits / BITS_PER_BYTE;  // each int is 4 bytes
 
         int n = a.length;
         int[] aux = new int[n];
@@ -70,7 +70,7 @@ public class LSD {
             // compute frequency counts
             int[] count = new int[R + 1];
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 count[c + 1]++;
             }
 
@@ -93,7 +93,7 @@ public class LSD {
 
             // move data
             for (int i = 0; i < n; i++) {
-                int c = (a[i] >> BITS_PER_BYTE * d) & MASK;
+                int c = (a[i] >> BITS_PER_BYTE * d) & mask;
                 aux[count[c]++] = a[i];
             }
 
