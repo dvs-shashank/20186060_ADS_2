86e762941fb2d66015454a660c5fcefd548705b8
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-12 14:59:11 +0530

    "ads2_m13_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m13/Radix Sorts/LSD.java
+++ m13/Radix Sorts/LSD.java
@@ -29,20 +29,24 @@ public class LSD {
 
             // compute frequency counts
             int[] count = new int[R + 1];
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 count[a[i].charAt(d) + 1]++;
+            }
 
             // compute cumulates
-            for (int r = 0; r < R; r++)
+            for (int r = 0; r < R; r++) {
                 count[r + 1] += count[r];
+            }
 
             // move data
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 aux[count[a[i].charAt(d)]++] = a[i];
+            }
 
             // copy back
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < n; i++) {
                 a[i] = aux[i];
+            }
         }
     }
 
--- m13/Radix Sorts/Solution.java
+++ m13/Radix Sorts/Solution.java
@@ -4,32 +4,31 @@ import java.util.Arrays;
  * Class for solution.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
-		//unused constructor.
-	}
-	/**
-	 * main method.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		Scanner rashmika = new Scanner(System.in);
-		int numOfInputs = Integer.parseInt(rashmika.nextLine());
-		String[] eachInput = new String[numOfInputs];
-		for (int i = 0; i < eachInput.length; i++) {
-			eachInput[i] = rashmika.nextLine();
-		}
-		//while(numOfInputs > 0) {
-		// 	eachInput[numOfInputs] = rashmika.nextLine();
-		// 	numOfInputs--;
-		// }
-		LSD lsdObj = new LSD();
-		lsdObj.sort(eachInput, eachInput[0].length());
-		System.out.println(		Arrays.toString(eachInput)
-		                  );
-		//System.out.println(msdObject);
-	}
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        //unused constructor.
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner rashmika = new Scanner(System.in);
+        int numOfInputs = Integer.parseInt(rashmika.nextLine());
+        String[] eachInput = new String[numOfInputs];
+        for (int i = 0; i < eachInput.length; i++) {
+            eachInput[i] = rashmika.nextLine();
+        }
+        //while(numOfInputs > 0) {
+        //  eachInput[numOfInputs] = rashmika.nextLine();
+        //  numOfInputs--;
+        // }
+        LSD lsdObj = new LSD();
+        lsdObj.sort(eachInput, eachInput[0].length());
+        System.out.println(Arrays.toString(eachInput));
+        //System.out.println(msdObject);
+    }
 }
