b551cea8a00e8f45b6bf14c9128ccb314685b85d
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-18 13:20:25 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- m10/Seam Carving/SeamCarver.java
+++ m10/Seam Carving/SeamCarver.java
@@ -15,13 +15,13 @@ public class SeamCarver {
     /**
      * Constructs the object.
      *
-     * @param      picture  The picture
+     * @param      piicture  The picture
      */
-    public SeamCarver(final Picture picture) {
-        if (picture == null) {
+    public SeamCarver(final Picture piicture) {
+        if (piicture == null) {
             throw new java.lang.IllegalArgumentException("picture is null");
         }
-        this.picture = new Picture(picture);
+        this.picture = new Picture(piicture);
     }
 
 
@@ -64,7 +64,8 @@ public class SeamCarver {
     public  double energy(final int x, final int y) {
         int w = width() - 1, h = height() - 1;
         if (x < 0 || x > w || y < 0 || y > h) {
-            throw new java.lang.IllegalArgumentException("IllegalArgumentException");
+            throw new
+            java.lang.IllegalArgumentException("IllegalArgumentException");
         }
         if (x == 0 || x == w ||  y == 0 || y == h) {
             return BORDER;
@@ -73,7 +74,7 @@ public class SeamCarver {
     }
 
     /**
-     * // energy of pixel at column x and row y not on boarder
+     * // energy of pixel at column x and row y not on boarder.
      *
      * @param      x  integer
      * @param      y  integer
@@ -191,7 +192,7 @@ public class SeamCarver {
                 path[row] = col - 1;
             }
             if (col < (w - 2) && energies[row][col + 1]
-                < energies[row][path[row]]) {
+                    < energies[row][path[row]]) {
                 path[row] = col + 1;
             }
         }
@@ -206,11 +207,11 @@ public class SeamCarver {
         double[][] transposeEnergies = transposeGrid(initEnergies());
         return minVerticalPath(transposeEnergies);
     }
-   /**
-    *  // sequence of indices for vertical seam.
-    *
-    * @return   // sequence of indices for vertical seam.
-    */
+    /**
+     *  // sequence of indices for vertical seam.
+     *
+     * @return   // sequence of indices for vertical seam.
+     */
     public int[] findVerticalSeam() {
         double[][] normalEnergies = initEnergies();
         return minVerticalPath(normalEnergies);
@@ -264,7 +265,15 @@ public class SeamCarver {
         this.picture = pic;
     }
 
-    // return false if two consecutive entries differ by more than 1
+    /**
+     * Determines if valid.
+     *
+     * @param      a      { parameter_description }
+     * @param      len    The length
+     * @param      range  The range
+     *
+     * @return     True if valid, False otherwise.
+     */
     private boolean isValid(final int[] a, final int len, final int range) {
         if (a == null) {
             return false;
@@ -273,8 +282,9 @@ public class SeamCarver {
             return false;
         }
         for (int i = 1; i < len; i++) {
-            if (a[i] < Math.max(0, a[i - 1] - 1) ||
-                a[i] > Math.min(range, a[i - 1] + 1))
+            if (a[i] < Math.max(0, a[i - 1] - 1)
+                    ||
+                    a[i] > Math.min(range, a[i - 1] + 1))
                 return false;
         }
         return true;
