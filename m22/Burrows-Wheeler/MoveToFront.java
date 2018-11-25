a283b3e0543ef2666be0dedf954615433a3289c9
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-25 23:00:53 +0530

    m22

--- m22/Burrows-Wheeler/MoveToFront.java
+++ m22/Burrows-Wheeler/MoveToFront.java
@@ -1,25 +1,48 @@
-/**
- * Class for move to front.
- */
+import java.util.LinkedList;
+import edu.princeton.cs.algs4.BinaryStdIn;
+import edu.princeton.cs.algs4.BinaryStdOut;
 public class MoveToFront {
-	/**
-	 * { function_description }.
-	 */
+    private static final int RADIX = 256;
     public static void encode() {
-    	
+        String input = BinaryStdIn.readString();
+        char[] array = input.toCharArray();
+        LinkedList<Integer> list = new LinkedList<Integer>();
+        for (int i = 0; i < RADIX; i++) {
+            list.add(i);
+        }
+        for (int j = 0; j < array.length; j++) {
+            int index = list.indexOf((int) array[j]);
+            BinaryStdOut.write((char) index, 8);
+            int x = list.remove(index);
+            list.add(0, x);
+        }
+        BinaryStdOut.close();
     }
-    /**
-     * { function_description }.
-     */
+
     public static void decode() {
-    	
+        String input1 = BinaryStdIn.readString();
+        char[] array1 = input1.toCharArray();
+        LinkedList<Integer> list1 = new LinkedList<Integer>();
+        for (int k = 0; k < RADIX; k++) {
+            list1.add(k);
+        }
+        for (int l = 0; l < array1.length; l++) {
+            int y = list1.remove((int) array1[l]);
+            list1.add(0, y);
+            BinaryStdOut.write((char) y, 8);
+        }
+        BinaryStdOut.close();
     }
-    /**
-     * main method.
-     *
-     * @param      args  The arguments
-     */
-    public static void main(final String[] args) {
-    	
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            return;
+        }
+        if (args[0].equals("-")) {
+            encode();
+        }
+        if (args[0].equals("+")) {
+            decode();
+        }
     }
 }
