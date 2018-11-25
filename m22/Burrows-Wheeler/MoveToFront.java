b6dcfa17d6d51e8b4b88161895202df696c2d7ea
dvs-shashank <dvs.shashank6@msitprogram.net>
2018-11-25 22:15:19 +0530

    m24

---
+++ m22/Burrows-Wheeler/MoveToFront.java
@@ -0,0 +1,25 @@
+/**
+ * Class for move to front.
+ */
+public class MoveToFront {
+	/**
+	 * { function_description }.
+	 */
+    public static void encode() {
+    	
+    }
+    /**
+     * { function_description }.
+     */
+    public static void decode() {
+    	
+    }
+    /**
+     * main method.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+    	
+    }
+}
